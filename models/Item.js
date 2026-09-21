a50c4d0938b3c63aa5c32adb4caebe27421a98a1
Sayed-2003 <baqerglopgamer@gmail.com>
2026-09-21 03:39:42 +0300

    created the Item model

---
+++ models/Item.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+
+    description: {
+        type: String,
+        required: true,
+        maxlength: 500
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        required: true
+    },
+
+    type: {
+        type: String,
+        enum: ['Lost', 'Found'],
+        required: true
+    },
+    image:{
+        type:String
+    },
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {timestamps: true})
+
+const Item = mongoose.model('Item', itemSchema)
+
+module.exports = Item
