9c0d882a7d33f6333431f486c83283027c552c36
Sayed-2003 <baqerglopgamer@gmail.com>
2026-09-21 04:12:58 +0300

    added post route for the creation form

--- routes/items-routes.js
+++ routes/items-routes.js
@@ -1,8 +1,32 @@
 const router = require('express').Router()
+const isSignedIn = require('../middleware/is-signed-in');
 const Item =require("../models/Item")
 
 router.get('/create', (req, res) => {
     res.render('items/create-item.ejs')
 })
+
+router.post('/create',isSignedIn, async (req, res) => {
+    try {
+        const{title,description,category,location,date,type,image} = req.body
+        const createdItem = await Item.create({
+            title: title,
+            description: description,
+            category: category,
+            location: location,
+            date: date,
+            type: type,
+            image: image,
+            owner: req.session.user._id
+        })
+
+        console.log(createdItem);
+
+        res.redirect('/items/create');
+
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 module.exports = router
