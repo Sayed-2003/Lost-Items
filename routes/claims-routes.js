c9bb7a6df5644585a96afa5d0c1d8d21ea349a29
Sayed-2003 <baqerglopgamer@gmail.com>
2026-09-22 05:52:56 +0300

    added the get and post route for claim form

--- routes/claims-routes.js
+++ routes/claims-routes.js
@@ -1,7 +1,35 @@
 const router = require('express').Router()
 const isSignedIn = require('../middleware/is-signed-in')
 const Claim = require('../models/Claim')
+const Item = require('../models/Item')
 
+router.get('/create/:itemId', isSignedIn, async (req, res) => {
+    try {
+        const foundItem = await Item.findById(req.params.itemId)
+
+        res.render('claims/create-claim.ejs', {item: foundItem})
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.post('/create/:itemId', isSignedIn, async (req, res) => {
+    try {
+        const foundItem = await Item.findById(req.params.itemId);
+
+        const newClaim = await Claim.create({
+            item: foundItem._id,
+            claimant: req.session.user._id,
+            message: req.body.message
+        });
+
+        res.redirect('/items/all-items');
+
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
 
